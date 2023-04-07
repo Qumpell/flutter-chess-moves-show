7b33eb9127ee74f0cd5c6e303d4bc858cf98ce09
Qumpell <qqumpel@gmail.com>
2023-04-07 12:23:22 +0200

    adding navigation to next page

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 void main() {
-  runApp(MainApp());
+  runApp(const MainApp());
 }
 
 class MovesController extends GetxController {
@@ -11,6 +11,7 @@ class MovesController extends GetxController {
 
   void updateChessboard(String newMove) {
     moveName.value = newMove;
+    moveName.refresh();
   }
 
   MovesController(int chessboardNumber) {
@@ -18,18 +19,14 @@ class MovesController extends GetxController {
   }
 }
 
-
-
-
-
 class MainApp extends StatelessWidget {
+  const MainApp({super.key});
 
-
-@override
-  Widget build(BuildContext context){
+  @override
+  Widget build(BuildContext context) {
     Widget chessboardSection = Container(
       child: Row(
-         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: <Widget>[
           GetX<MovesController>(
             tag: "1",
@@ -37,85 +34,70 @@ class MainApp extends StatelessWidget {
             builder: ((controller) {
               return Column(
                 children: <Widget>[
-                  Text(controller.moveName.value,
-                  style: TextStyle(fontSize: 24),)
+                  Text(
+                    controller.moveName.value,
+                    style: TextStyle(fontSize: 24),
+                  )
                 ],
-              );  
+              );
             }),
           ),
-          
-        ],
-      ),
-    )
-  }
-
-
-
-  @override
-  Widget build(BuildContext context){
-    Widget buttonSection = Container(
-      child: Row(
-         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: <Widget>[
-          ElevatedButton(
-            onPressed: () async{
-               final result = await Get.to(
-                () => ButtonPage(chessboardNumber: 1)
-            );
-            }
-          , child: Text("Pierwsza szachownica")),
         ],
       ),
     );
-  }
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: ListView(
-          children: [
-            chessboardSection,
-            buttonSection,
-          ],
-        )
+    Widget buttonSection = Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          ElevatedButton(
+              onPressed: () async {
+                final result =
+                    await Get.to(() => ButtonPage(chessboardNumber: 1));
+              },
+              child: Text("Pierwsza szachownica")),
+        ],
       ),
+    );
+
+    return GetMaterialApp(
+      home: Scaffold(
+          body: ListView(
+        children: [
+          chessboardSection,
+          buttonSection,
+        ],
+      )),
     );
   }
 }
-
 
 class ButtonPage extends StatelessWidget {
   final int chessboardNumber;
   final TextEditingController _textEditingController = TextEditingController();
   ButtonPage({required this.chessboardNumber});
-
+  final MovesController movesController = Get.put(MovesController(1));
   @override
-  Widget build(BuildContext context){
-    return Container(
-      child: 
-        GetX<MovesController>(
-          tag: chessboardNumber.toString(),
-          init: MovesController(chessboardNumber),
-          builder: (controller){
-            return Column(
-        children: <Widget>[
-          TextField(
-            controller: _textEditingController,
-            decoration: InputDecoration(labelText: "Enter fen"),
-          ),
-          ElevatedButton(onPressed: 
-          (){
-            controller.updateChessboard(_textEditingController.text);
-
-          }, child: Text("Update moves"))
-        ],
-      ), 
-          },
-        )
-      
-      
-      
+  Widget build(BuildContext context) {
+    return Card(
+      child: Container(
+        width: 300,
+        height: 300,
+        child: Column(
+          children: <Widget>[
+            TextField(
+              controller: _textEditingController,
+              decoration: InputDecoration(labelText: "Enter fen"),
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  movesController.updateChessboard(_textEditingController.text);
+                  Get.back(result: "WElcome back");
+                },
+                child: Text("Update moves"))
+          ],
+        ),
+      ),
     );
   }
 }
